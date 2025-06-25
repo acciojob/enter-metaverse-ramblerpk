7c8745c137736e864060abebe1d3ec7d0ea5a916
Piyush Kumar <109018436+ramblerpk@users.noreply.github.com>
2025-06-26 02:21:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+function meta(){
+	document.getElementById("status").innerHTML = "Entered Metaverse";
+}
+document.getElementById("enterBtn").addEventListener("click",meta);
