b30042e8633665fa1ce84fa39853cd95564340e2
Piyush Kumar <109018436+ramblerpk@users.noreply.github.com>
2025-06-26 02:29:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
 function meta(){
-	document.getElementById("status").innerHTML = "Entered Metaverse";
+	document.getElementById("status").innerHTML = "<h1>Entered Metaverse</h1>";
 }
 document.getElementById("enterBtn").addEventListener("click",meta);
